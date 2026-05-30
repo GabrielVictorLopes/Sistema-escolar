880b535faec26b9541f73cf067eca39b7e589f70
Fernando Santos <fnandojrs@hotmail.com>
2026-05-30 03:25:04 -0300

    Criando rota aluno

--- src/app/pages/aluno/aluno.ts
+++ src/app/pages/aluno/aluno.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-aluno',
@@ -9,6 +10,7 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './aluno.css',
 })
 export class Aluno {
+  constructor(private http: HttpClient) {}
 
   nome = '';
   cpf = '';
@@ -72,35 +74,64 @@ export class Aluno {
     }
   }
 
-  cadastrar() {
+cadastrar() {
 
-    // validação nome
-    if (!this.nome.trim()) {
-      alert('Informe o nome do aluno');
-      return;
-    }
-
-    // validação cpf
-    if (this.cpf.length !== 11) {
-      alert('CPF inválido');
-      return;
-    }
-
-    // validação telefone mãe
-    if (this.telMae && this.telMae.length < 10) {
-      alert('Telefone da mãe inválido');
-      return;
-    }
-
-    // validação telefone pai
-    if (this.telPai && this.telPai.length < 10) {
-      alert('Telefone do pai inválido');
-      return;
-    }
-
-    alert('Aluno cadastrado com sucesso!');
+  // validação nome
+  if (!this.nome.trim()) {
+    alert('Informe o nome do aluno');
+    return;
   }
 
+  // validação cpf
+  if (this.cpf.length !== 11) {
+    alert('CPF inválido');
+    return;
+  }
+
+  // validação telefone mãe
+  if (this.telMae && this.telMae.length < 10) {
+    alert('Telefone da mãe inválido');
+    return;
+  }
+
+  // validação telefone pai
+  if (this.telPai && this.telPai.length < 10) {
+    alert('Telefone do pai inválido');
+    return;
+  }
+
+  const dados = {
+    nome: this.nome,
+    cpf: this.cpf,
+    rg: this.rg,
+    sexo: this.sexo,
+    nomeMae: this.nomeMae,
+    telMae: this.telMae,
+    nomePai: this.nomePai,
+    telPai: this.telPai,
+    medicamento: this.medicamento,
+    observacao: this.observacao,
+    rua: this.rua,
+    bairro: this.bairro,
+    numero: this.numero,
+    cep: this.cep,
+    complemento: this.complemento
+  };
+
+  this.http.post(
+    'https://sistema-escolar-api-production.up.railway.app/aluno',
+    dados
+  ).subscribe({
+    next: () => {
+      alert('Aluno cadastrado com sucesso!');
+      this.cancelar();
+    },
+    error: (erro) => {
+      console.error(erro);
+      alert('Erro ao cadastrar aluno');
+    }
+  });
+}
   cancelar() {
     this.nome = '';
     this.cpf = '';
