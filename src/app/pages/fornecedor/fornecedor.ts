7acb3145d247b549eebfd83cd5424df4a93aa6e9
Fernando Santos <fnandojrs@hotmail.com>
2026-05-30 02:58:23 -0300

    Integração fornecedor com API Railway

--- src/app/pages/fornecedor/fornecedor.ts
+++ src/app/pages/fornecedor/fornecedor.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-fornecedor',
@@ -9,6 +10,7 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './fornecedor.css',
 })
 export class Fornecedor {
+  constructor(private http: HttpClient) {}
 
   empresa = '';
   cnpj = '';
@@ -69,13 +71,41 @@ export class Fornecedor {
   }
   cadastrar() {
 
-    if (this.cnpj.length !== 14) {
-      alert('CNPJ inválido');
-      return;
-    }
-
-    alert('Fornecedor cadastrado com sucesso!');
+  if (this.cnpj.length !== 14) {
+    alert('CNPJ inválido');
+    return;
   }
+
+  const dados = {
+    empresa: this.empresa,
+    cnpj: this.cnpj,
+    responsavel: this.responsavel,
+    telefone: this.telefone,
+    email: this.email,
+    produto: this.produto,
+    observacao: this.observacao,
+    rua: this.rua,
+    bairro: this.bairro,
+    numero: this.numero,
+    cep: this.cep,
+    complemento: this.complemento
+  };
+
+  this.http.post(
+    'https://sistema-escolar-api-production.up.railway.app/fornecedor',
+    dados
+  ).subscribe({
+    next: () => {
+      alert('Fornecedor cadastrado com sucesso!');
+      this.cancelar();
+    },
+    error: (erro) => {
+      console.error(erro);
+      alert('Erro ao cadastrar fornecedor');
+    }
+  });
+
+}
 
   cancelar() {
 
